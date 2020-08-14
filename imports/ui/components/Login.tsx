7a4bd74cd367ebf1369ea2b74a37ad49ef4eaac7
Pierre <projet.troiscentsoixante@gmail.com>
2020-08-14 20:31:11 +0200

    USERLOGIN

---
+++ imports/ui/components/Login.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import  { Meteor } from 'meteor/meteor'
+import { Accounts } from 'meteor/accounts-base';
+
+import RightImg from "./RightImg"
+import FormLogin from "./FormLogin"
+
+const messageText:string = "Connectez vous afin de lancer une conversation"
+
+const Login = (props:any):JSX.Element => {
+  const handleLogin = (state:any):void => {
+    const {password, username} = state
+    Meteor.call('user.login', state, (err, res) => {
+      if(err) {
+        console.log('erreur du login', err);
+      } else {
+        console.log('resultat', res);
+        Meteor.loginWithPassword(username, password, (err) => {
+          if(err) {
+            console.log('err', err);
+          }else {
+            console.log('res', res);
+            props.history.push('/chats')
+          }
+        })
+      }
+    })
+  }
+  return (
+    <RightImg messageText={messageText}>
+      <FormLogin onLogin={handleLogin}></FormLogin>
+    </RightImg>
+  )
+};
+
+export default Login;
