0a9b1c126e76efb20523580ac5ad59173a0e1bed
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2021-01-31 22:12:52 +0100

    75 fin

--- imports/ui/components/MessageBox.tsx
+++ imports/ui/components/MessageBox.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import FontAwesome from 'react-fontawesome';
+import _ from 'lodash';
+import moment from 'moment';
 
 import StyledMessageBox from '../elements/StyledMessageBox';
+import Day from './Day';
+import MessageText from './MessageText';
 
 let isEven:boolean = false;
+const format:string ="D MMMM Y";
 
 const MessageBox = (props:any):JSX.Element => {
   const {messages} = props;
-  console.log('messages', messages);
   messages.forEach(message =>{
     if(!message.senderId) {
       message.ownership = !!message.ownership === isEven ? 'mine' : 'other';
@@ -19,10 +23,48 @@ const MessageBox = (props:any):JSX.Element => {
       return message;
     }
   })
-  console.log('message avec ownership', messages)
+  //console.log('message avec ownership', messages)
+  const groupedMessages:any = _.groupBy(messages, message => {
+    return moment(message.createdAt).format(format);    
+  })
+ // console.log('groupedMessages', groupedMessages )
+  const newMessages:any[] = Object.keys(groupedMessages)
+  .map(key => {
+    return {
+      date: key,
+      groupedMessages: groupedMessages[key],
+      today: moment().format(format) === key
+    }
+  })
+  const renderMessages = (newMessage:any):JSX.Element[] => {
+    return newMessage.groupedMessages.map(message => {
+      const msgClass:string = `message message--${message.ownership}`
+      return ( 
+        <MessageText
+        key={message._id}
+        msgClass={msgClass}
+        content={message.content}
+        ownership={message.ownership} 
+        />
+      )
+    })
+  }
+   
+  const renderDays = ():JSX.Element[] => {
+    return newMessages.map((newMessage, index:number) => {
+      const dateText:string = newMessage.today ? "aujourd'hui" : newMessage.date ;
+      return (
+        <div key={index}>
+          <Day date={newMessage.date}/>
+          {renderMessages(newMessage)}
+        </div>
+      )
+    })
+  }
+//console.log('newMessages', newMessages )
   return (
     <StyledMessageBox>
-kjbgkjgk
+{renderDays()}
     </StyledMessageBox>
   )
 };
