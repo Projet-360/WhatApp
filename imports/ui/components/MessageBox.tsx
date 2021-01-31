d077085f3753cb9789acda9c7f94f22f55853795
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2021-01-31 21:01:32 +0100

    71 fin

--- imports/ui/components/MessageBox.tsx
+++ imports/ui/components/MessageBox.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import FontAwesome from 'react-fontawesome';
 
 import StyledMessageBox from '../elements/StyledMessageBox';
 
+let isEven:boolean = false;
+
 const MessageBox = (props:any):JSX.Element => {
   const {messages} = props;
   console.log('messages', messages);
+  messages.forEach(message =>{
+    if(!message.senderId) {
+      message.ownership = !!message.ownership === isEven ? 'mine' : 'other';
+      isEven= !isEven;
+      return message;
+    }else{
+      message.ownership = message.senderId === Meteor.userId() ? 'mine' : 'other';
+      return message;
+    }
+  })
+  console.log('message avec ownership', messages)
   return (
     <StyledMessageBox>
 kjbgkjgk
